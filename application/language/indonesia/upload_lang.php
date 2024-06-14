40440a45e9672d183740bd079001a0f7cab88602
dvpms <devranmalik82@gmail.com>
2024-06-14 16:48:02 +0700

    terbaru boss

---
+++ application/language/indonesia/upload_lang.php
@@ -0,0 +1,17 @@
+<?php
+$lang['upload_userfile_not_set'] = "Tidak dapat menemukan variabel pos yang disebut userfile.";
+$lang['upload_file_exceeds_limit'] = "Berkas yang diunggah melebihi ukuran maksimum yang diperbolehkan dalam file konfigurasi PHP Anda.";
+$lang['upload_file_exceeds_form_limit'] = "Berkas yang diunggah melebihi ukuran maksimum yang diperbolehkan oleh formulir pengajuan.";
+$lang['upload_file_partial'] = "Berkas hanya terunggah sebagian.";
+$lang['upload_no_temp_directory'] = "Folder sementara hilang.";
+$lang['upload_unable_to_write_file'] = "Berkas tidak dapat ditulis ke disk.";
+$lang['upload_stopped_by_extension'] = "Unggahan berkas dihentikan oleh ekstensi.";
+$lang['upload_no_file_selected'] = "Anda tidak memilih berkas untuk diunggah.";
+$lang['upload_invalid_filetype'] = "Jenis berkas yang Anda coba unggah tidak diperbolehkan.";
+$lang['upload_invalid_filesize'] = "Berkas yang Anda coba unggah lebih besar dari ukuran yang diizinkan.";
+$lang['upload_invalid_dimensions'] = "Gambar yang Anda coba unggah melebihi tinggi atau lebar maksimum.";
+$lang['upload_destination_error'] = "Terjadi masalah saat mencoba memindahkan berkas yang diunggah ke tujuan akhir.";
+$lang['upload_no_filepath'] = "Jalur unggahan tampaknya tidak valid.";
+$lang['upload_no_file_types'] = "Anda belum menentukan jenis berkas yang diperbolehkan.";
+$lang['upload_bad_filename'] = "Nama berkas yang Anda kirimkan sudah ada di server.";
+$lang['upload_not_writable'] = "Folder tujuan unggahan tampaknya tidak dapat ditulisi.";
