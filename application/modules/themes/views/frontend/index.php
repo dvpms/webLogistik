2dd307f4d54e93acc20e91206c3a63b6405ea43e
dvpms <devranmalik82@gmail.com>
2024-07-05 18:10:02 +0700

    Update index.php

--- application/modules/themes/views/frontend/index.php
+++ application/modules/themes/views/frontend/index.php
@@ -12,7 +12,7 @@
 				<div class="logo">Logo</div>
 				<ul>
 					<li><a href="<?php echo base_url('home'); ?>">Home</a></li>
-					<li><a href="<?php echo base_url('home'); ?>">About</a></li>
+					<li><a href="<?php echo base_url('about'); ?>">About</a></li>
 					<li><a href="<?php echo base_url('service'); ?>">Services</a></li>
 					<li><a href="<?php echo base_url('pricing'); ?>">Pricing</a></li>
 					<li><a href="<?php echo base_url('history'); ?>">History</a></li>
