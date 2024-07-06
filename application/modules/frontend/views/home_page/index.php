d91582fbaa41b5a6eeaf0be0be4bb4e9b26727e2
dvpms <devranmalik82@gmail.com>
2024-07-06 14:39:46 +0700

    add home page and setup views on frontend

---
+++ application/modules/frontend/views/home_page/index.php
@@ -0,0 +1,21 @@
+<section class="hero">
+	<div class="hero-content">
+		<h2>WHO WE ARE</h2>
+		<h1>Every distance is valuable</h1>
+		<p>On-time Delivery, Best Service - Wherever and Whenever You Need It</p>
+		<ul>
+			<li>Prioritization of speed and accuracy</li>
+			<li>Professional customer service</li>
+			<li>Wide delivery range that covers various regions, even remote locations</li>
+		</ul>
+	</div>
+	<div class="hero-images">
+		<div class="main-image-container">
+			<img src="/frond-end//images/delivery-man.png" alt="Delivery Man" class="main-image">
+		</div>
+		<div class="secondary-images">
+			<img src="/frond-end//images/delivery-woman.png" alt="Delivery Woman" class="secondary-image">
+			<img src="/frond-end//images/package-handover.png" alt="Package Handover" class="secondary-image">
+		</div>
+	</div>
+ </section>
