8e010fcad08ca63dffcc1ff5f320a67495ed1dfd
dvpms <devranmalik82@gmail.com>
2024-07-06 12:56:21 +0700

    add home page

---
+++ application/modules/frontend/home_page/views/index.php
@@ -0,0 +1,21 @@
+<section class="hero">
+            <div class="hero-content">
+                <h2>WHO WE ARE</h2>
+                <h1>Every distance is valuable</h1>
+                <p>On-time Delivery, Best Service - Wherever and Whenever You Need It</p>
+                <ul>
+                    <li>Prioritization of speed and accuracy</li>
+                    <li>Professional customer service</li>
+                    <li>Wide delivery range that covers various regions, even remote locations</li>
+                </ul>
+            </div>
+            <div class="hero-images">
+                <div class="main-image-container">
+                    <img src="<?php echo base_url('public/assets/frondtend/images/delivery-man.png'); ?>" alt="Delivery Man" class="main-image">
+                </div>
+                <div class="secondary-images">
+                    <img src="<?php echo base_url('public/assets/frondtend/images/frondend/images/delivery-woman.png'); ?>" alt="Delivery Woman" class="secondary-image">
+                    <img src="<?php echo base_url('public/assets/frondtend/images/frondend/images/package-handover.png'); ?>" alt="Package Handover" class="secondary-image">
+                </div>
+            </div>
+</section>
