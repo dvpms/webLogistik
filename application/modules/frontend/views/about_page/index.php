90077eb93b6a514cea0d3a31853bffc307230346
dvpms <devranmalik82@gmail.com>
2024-07-07 17:21:26 +0700

    it's about page not home page

--- application/modules/frontend/views/about_page/index.php
+++ application/modules/frontend/views/about_page/index.php
@@ -11,11 +11,11 @@
 	</div>
 	<div class="hero-images">
 		<div class="main-image-container">
-			<img src="/frond-end//images/delivery-man.png" alt="Delivery Man" class="main-image">
+			<img src="public/assets/frontend/images/delivery-man.png" alt="Delivery Man" class="main-image">
 		</div>
 		<div class="secondary-images">
-			<img src="/frond-end//images/delivery-woman.png" alt="Delivery Woman" class="secondary-image">
-			<img src="/frond-end//images/package-handover.png" alt="Package Handover" class="secondary-image">
+			<img src="public/assets/frontend/images/delivery-woman.png" alt="Delivery Woman" class="secondary-image">
+			<img src="public/assets/frontend/images/package-handover.png" alt="Package Handover" class="secondary-image">
 		</div>
 	</div>
  </section>
