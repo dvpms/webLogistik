5c2bd3e25ac9c128fdd21090debd5f65f6917626
dvpms <devranmalik82@gmail.com>
2024-07-13 15:55:00 +0700

    add home page

---
+++ application/modules/frontend/views/home-page/index.php
@@ -0,0 +1,11 @@
+<section class="home-page">
+	<div class="hero">
+		<div class="hero-content">
+			<h1>Connecting Your Freight with Speed and Reliability</h1>
+			<p>We are dedicated to providing top-notch logistics and trucking solutions that ensure your freight arrives on time, every time. With years of industry experience, we pride ourselves on delivering exceptional service with a focus on speed, reliability, and efficiency.</p>
+		</div>
+		<div class="hero-image">
+			<img src="images/Logistics.png" alt="Logistics Illustration">
+		</div>
+	</div>
+</section>
