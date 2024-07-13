c07476272758fcce0528bc52c12683bc20ccdc0d
dvpms <devranmalik82@gmail.com>
2024-07-13 14:11:19 +0700

    update about, index, an services_details pages

--- application/modules/frontend/views/services_page/index.php
+++ application/modules/frontend/views/services_page/index.php
@@ -1,36 +1,36 @@
 <section class="services">
-            <h1>Services</h1>
-            <p>We serve what you needed</p>
-            <div class="service-grid">
-                <div class="service-card">
-                    <div class="icon"><img src="<?php echo base_url('public/assets/frontend/images/truck.svg')?>" alt=""> </div>
-                    <div class="content">
-                        <a href="<?php echo base_url('/index/services-details'); ?>">
-                            <h2>Pengiriman Dalam Kota (Reguler)</h2>
-                        </a>
-						<p>Pengiriman yang dapat dilatur untuk kota tertentu yang disesuaikan oleh lokasi anda dan dapat dilaksanakan dengan tepat</p>
-                    </div>
-                </div>
-                <div class="service-card">
-                    <div class="icon"><img src="<?php echo base_url('public/assets/frontend/images/truck.svg')?>" alt=""></div>
-                    <div class="content">
-                        <h2>Pengiriman Dalam Kota (Instant)</h2>
-                        <p>Pengiriman yang dapat dilatur untuk kota tertentu yang disesuaikan oleh lokasi anda dan dapat dilaksanakan dengan cepat</p>
-                    </div>
-                </div>
-                <div class="service-card">
-                    <div class="icon"><img src="<?php echo base_url('public/assets/frontend/images/truck.svg')?>" alt=""></div>
-                    <div class="content">
-                        <h2>Pengiriman Luar Kota (Reguler)</h2>
-                        <p>Pengiriman yang dapat mencakup seluruh wilayah yang ada di tempat anda tinggal</p>
-                    </div>
-                </div>
-                <div class="service-card">
-                    <div class="icon"><img src="<?php echo base_url('public/assets/frontend/images/truck.svg')?>" alt=""></div>
-                    <div class="content">
-                        <h2>Pengiriman Luar Kota (Instant)</h2>
-                        <p>Pengiriman yang dapat mencakup seluruh wilayah yang ada di tempat anda tinggal</p>
-                    </div>
-                </div>
-            </div>
-		</section>
+	<h1>Services</h1>
+	<p>We serve what you needed</p>
+	<div class="service-grid">
+		<div class="service-card">
+			<div class="icon"><img src="images/truck.svg" alt=""> </div>
+			<div class="content">
+				<a href="service-details-p.html">
+					<h2>Pengiriman Dalam Kota (Reguler)</h2>
+					<p>Pengiriman yang dapat dilatur untuk kota tertentu yang disesuaikan oleh lokasi anda dan dapat dilaksanakan dengan tepat</p>    
+				</a>
+			</div>
+		</div>
+		<div class="service-card">
+			<div class="icon"><img src="images/truck.svg" alt=""></div>
+			<div class="content">
+				<h2>Pengiriman Dalam Kota (Instant)</h2>
+				<p>Pengiriman yang dapat dilatur untuk kota tertentu yang disesuaikan oleh lokasi anda dan dapat dilaksanakan dengan cepat</p>
+			</div>
+		</div>
+		<div class="service-card">
+			<div class="icon"><img src="images/truck.svg" alt=""></div>
+			<div class="content">
+				<h2>Pengiriman Luar Kota (Reguler)</h2>
+				<p>Pengiriman yang dapat mencakup seluruh wilayah yang ada di tempat anda tinggal</p>
+			</div>
+		</div>
+		<div class="service-card">
+			<div class="icon"><img src="images/truck.svg" alt=""></div>
+			<div class="content">
+				<h2>Pengiriman Luar Kota (Instant)</h2>
+				<p>Pengiriman yang dapat mencakup seluruh wilayah yang ada di tempat anda tinggal</p>
+			</div>
+		</div>
+	</div>
+</section>
