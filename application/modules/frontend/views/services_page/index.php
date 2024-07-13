100d4f186b37b0d56e132eb1e13d8ab0e560581b
dvpms <devranmalik82@gmail.com>
2024-07-13 15:34:16 +0700

    mulai dari awal

--- application/modules/frontend/views/services_page/index.php
+++ application/modules/frontend/views/services_page/index.php
@@ -3,7 +3,7 @@
     <p>We serve what you needed</p>
     <div class="service-grid">
         <div class="service-card">
-            <div class="icon"><img src="images/truck.svg" alt=""> </div>
+            <div class="icon"><img src="<?php echo base_url('public/assets/frontend/images/truck.svg'); ?>" alt=""> </div>
             <div class="content">
                 <a href="<?php echo site_url('index/services/details')?>">
                     <h2>Pengiriman Dalam Kota (Reguler)</h2>
@@ -13,7 +13,7 @@
             </div>
         </div>
         <div class="service-card">
-            <div class="icon"><img src="images/truck.svg" alt=""></div>
+            <div class="icon"><img src="<?php echo base_url('public/assets/frontend/images/truck.svg'); ?>" alt=""></div>
             <div class="content">
                 <h2>Pengiriman Dalam Kota (Instant)</h2>
                 <p>Pengiriman yang dapat dilatur untuk kota tertentu yang disesuaikan oleh lokasi anda dan dapat
@@ -21,14 +21,14 @@
             </div>
         </div>
         <div class="service-card">
-            <div class="icon"><img src="images/truck.svg" alt=""></div>
+            <div class="icon"><img src="<?php echo base_url('public/assets/frontend/images/truck.svg'); ?>" alt=""></div>
             <div class="content">
                 <h2>Pengiriman Luar Kota (Reguler)</h2>
                 <p>Pengiriman yang dapat mencakup seluruh wilayah yang ada di tempat anda tinggal</p>
             </div>
         </div>
         <div class="service-card">
-            <div class="icon"><img src="images/truck.svg" alt=""></div>
+            <div class="icon"><img src="<?php echo base_url('public/assets/frontend/images/truck.svg'); ?>" alt=""></div>
             <div class="content">
                 <h2>Pengiriman Luar Kota (Instant)</h2>
                 <p>Pengiriman yang dapat mencakup seluruh wilayah yang ada di tempat anda tinggal</p>
