b8c7dc42e673c170b7c0723dea6a9b755a8e25ed
dvpms <devranmalik82@gmail.com>
2024-07-13 15:46:47 +0700

    service-details fixed

--- application/modules/frontend/views/services_details/index.php
+++ application/modules/frontend/views/services_details/index.php
@@ -1,101 +1,50 @@
 <section class="services-details">
-    <h1>Services Details</h1>
-    <div class="services-container">
-        <div class="services-list">
-            <h2>Services List</h2>
-            <hr>
-            <ul>
-                <li>
-                    <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                    Pengiriman Dalam Kota (Instant)
-                </li>
-                <li>
-                    <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                    Pengiriman Dalam Kota (Reguler)
-                </li>
-                <li>
-                    <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                    Pengiriman Luar Kota (Instant)
-                </li>
-                <li>
-                    <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                    Pengiriman Luar Kota (Reguler)
-                </li>
-            </ul>
-        </div>
-        <div class="delivery-map">
-            <img src="images/delivery-map.png" alt="Delivery Map">
-        </div>
-        <div class="contact-info">
-            <h2>Have a Question?</h2>
-            <img src="images/phone.png" alt="">
-            <span>+1 9876352081</span>
-        </div>
-        <div class="opportunity">
-            <h2>Opportunity</h2>
-            <ul>
-                <li>
-                    <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                    Fast delivery
-                </li>
-                <li>
-                    <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                    Right on time
-                </li>
-                <li>
-                    <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                    No frills, no extra charges
-                </li>
-            </ul>
-        </div>
-    </div>
+	<h1>Services Details</h1>
+	<div class="services-container">
+		<div class="services-list">
+			<h2>Services List</h2>
+			<hr>
+			<ul>
+				<li>
+					<img src="<?php echo base_url('public/assets/frontend/images/emojione-monotone--right-arrow.png'); ?>" alt="" style="vertical-align: middle;">
+					Pengiriman Dalam Kota (Instant)
+				</li>
+				<li>
+					<img src="<?php echo base_url('public/assets/frontend/images/emojione-monotone--right-arrow.png'); ?>" alt="" style="vertical-align: middle;">
+					Pengiriman Dalam Kota (Reguler)
+				</li>
+				<li>
+					<img src="<?php echo base_url('public/assets/frontend/images/emojione-monotone--right-arrow.png'); ?>" alt="" style="vertical-align: middle;">
+					Pengiriman Luar Kota (Instant)
+				</li>
+				<li>
+					<img src="<?php echo base_url('public/assets/frontend/images/emojione-monotone--right-arrow.png'); ?>" alt="" style="vertical-align: middle;">
+					Pengiriman Luar Kota (Reguler)
+				</li>
+			</ul>
+		</div>
+		<div class="delivery-map">
+			<img src="<?php echo base_url('public/assets/frontend/images/delivery-map.png'); ?>" alt="Delivery Map">
+		</div>
+		<div class="contact-info">
+			<h2>Have a Question?</h2>
+			<img src="images/phone.png" alt="">
+			<span>+1 9876352081</span>
+		</div>
+		<div class="opportunity">
+			<h2>Opportunity</h2>
+			<ul>
+				<li>
+					<img src="<?php echo base_url('public/assets/frontend/images/emojione-monotone--right-arrow.png'); ?>" alt="" style="vertical-align: middle;">
+					Fast delivery</li>
+				<li>
+					<img src="<?php echo base_url('public/assets/frontend/images/emojione-monotone--right-arrow.png'); ?>" alt="" style="vertical-align: middle;">
+					Right on time
+				</li>
+				<li>
+					<img src="<?php echo base_url('public/assets/frontend/images/emojione-monotone--right-arrow.png'); ?>" alt="" style="vertical-align: middle;">
+					No frills, no extra charges</li>
+			</ul>
+		</div>
+	</div>
 </section>
-        <h1>Services Details</h1>
-        <div class="services-container">
-            <div class="services-list">
-                <h2>Services List</h2>
-                <hr>
-                <ul>
-                    <li>
-                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                        Pengiriman Dalam Kota (Instant)
-                    </li>
-                    <li>
-                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                        Pengiriman Dalam Kota (Reguler)
-                    </li>
-                    <li>
-                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                        Pengiriman Luar Kota (Instant)
-                    </li>
-                    <li>
-                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                        Pengiriman Luar Kota (Reguler)
-                    </li>
-                </ul>
-            </div>
-            <div class="delivery-map">
-                <img src="images/delivery-map.png" alt="Delivery Map">
-            </div>
-            <div class="contact-info">
-                <h2>Have a Question?</h2>
-                <img src="images/phone.png" alt="">
-                <span>+1 9876352081</span>
-            </div>
-            <div class="opportunity">
-                <h2>Opportunity</h2>
-                <ul>
-                    <li>
-                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                        Fast delivery</li>
-                    <li>
-                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                        Right on time
-                    </li>
-                    <li>
-                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
-                        No frills, no extra charges</li>
-                </ul>
-            </div>
-        </div>
-    </section>
