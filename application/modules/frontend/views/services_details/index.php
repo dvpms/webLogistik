59cdedb9150bf409ab3741e681ea8a187fdc254d
dvpms <devranmalik82@gmail.com>
2024-07-13 15:22:47 +0700

    reverse

--- application/modules/frontend/views/services_details/index.php
+++ application/modules/frontend/views/services_details/index.php
@@ -57,43 +57,43 @@
                 <hr>
                 <ul>
                     <li>
-                        <img src="<?php echo base_url('emojione-monotone--right-arrow.png')?>" alt="" style="vertical-align: middle;">
+                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
                         Pengiriman Dalam Kota (Instant)
                     </li>
                     <li>
-                        <img src="<?php echo base_url('images/emojione-monotone--right-arrow.png')?>" alt="" style="vertical-align: middle;">
+                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
                         Pengiriman Dalam Kota (Reguler)
                     </li>
                     <li>
-                        <img src="<?php echo base_url('images/emojione-monotone--right-arrow.png')?>" alt="" style="vertical-align: middle;">
+                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
                         Pengiriman Luar Kota (Instant)
                     </li>
                     <li>
-                        <img src="<?php echo base_url('images/emojione-monotone--right-arrow.png')?>" alt="" style="vertical-align: middle;">
+                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
                         Pengiriman Luar Kota (Reguler)
                     </li>
                 </ul>
             </div>
             <div class="delivery-map">
-                <img src="<?php echo base_url('public/assets/frontend/images/delivery-map.png')?>" alt="Delivery Map">
+                <img src="images/delivery-map.png" alt="Delivery Map">
             </div>
             <div class="contact-info">
                 <h2>Have a Question?</h2>
-                <img src="<?php echo base_url('public/assets/frontend/images/phone.png')?>" alt="">
+                <img src="images/phone.png" alt="">
                 <span>+1 9876352081</span>
             </div>
             <div class="opportunity">
                 <h2>Opportunity</h2>
                 <ul>
                     <li>
-                        <img src="<?php echo base_url('public/assets/frontend/images/emojione-monotone--right-arrow.png')?>" alt="" style="vertical-align: middle;">
+                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
                         Fast delivery</li>
                     <li>
-                        <img src="<?php echo base_url('public/assets/frontend/images/emojione-monotone--right-arrow.png')?>" alt="" style="vertical-align: middle;">
+                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
                         Right on time
                     </li>
                     <li>
-                        <img src="<?php echo base_url('public/assets/frontend/images/emojione-monotone--right-arrow.png')?>" alt="" style="vertical-align: middle;">
+                        <img src="images/emojione-monotone--right-arrow.png" alt="" style="vertical-align: middle;">
                         No frills, no extra charges</li>
                 </ul>
             </div>
